817cff9df734c34f0209cd2ad7d6079f0273f1b1
Truman Hung <truman.hung@sap.com>
2021-03-06 03:11:29 -0800

    Capture selected text in popup input

--- popup.js
+++ popup.js
@@ -1,10 +1,11 @@
+const GitHubLink = "https://github.com/dchen150/ELI5-chrome-extension";
+
 /* The function that finds and returns the selected text */
 const getSelectedText = function () {
     const {baseOffset, focusOffset} = window.getSelection();
     const selected = Math.abs(baseOffset - focusOffset) > 0;
-    const selectedText = selected ? window.getSelection().getRangeAt(0).cloneContents().textContent : ''
-    console.log(selectedText);
-    return selectedText;
+    const selectedText = selected ? window.getSelection().getRangeAt(0).cloneContents().textContent : null
+    chrome.storage.sync.set({ selectedText }); // storing the selected text globally
 };
 
 /* This line converts the above function to string
@@ -18,6 +19,15 @@ window.addEventListener('DOMContentLoaded', function () {
     chrome.tabs.executeScript({
         code: jsCodeWrapper(getSelectedText)
     });
+
+    const header = document.querySelector(".header")
+    header.addEventListener('click', () => chrome.tabs.create({url: GitHubLink}));
+
+    const searchTerm = document.querySelector('.searchTerm');
+    chrome.storage.sync.get(['selectedText'], function(result) {
+        searchTerm.value = result.selectedText;
+    });
+
 
     // Search
     const searchBtn = document.querySelector('.searchButton')
