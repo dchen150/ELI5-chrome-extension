94bc7321358fb0a89309bd09208f78ef53dd7745
Derek Chen <45786074+dchen150@users.noreply.github.com>
2021-03-06 14:11:30 -0800

    pushing node fetch and reddit progress

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,5 +1,6 @@
 const functions = require("firebase-functions");
 const language = require("@google-cloud/language");
+const fetch = require("node-fetch");
 
 const findEntitiesHandler = async (request, response) => {
   const client = new language.LanguageServiceClient();
@@ -12,9 +13,39 @@ const findEntitiesHandler = async (request, response) => {
   const [result] = await client.analyzeEntities({document});
 
   const entities = result.entities;
-  response.send(200, {
-    entities,
+
+  const max = entities.reduce(function(prev, current) {
+    return (prev.salience > current.salience) ? prev : current;
   });
+
+  const res = {
+    reddit: searchReddit(max, "relevance", 5),
+  };
+  // searchStackOverFlow(max);
+  response.send(200, res);
+};
+
+const searchReddit = async (max, sortBy, limit) => {
+  return fetch(`http://www.reddit.com/search.json?q=${max.name} ELI5&sort=${sortBy}&limit=${limit}`)
+      .then((res) => res.json())
+      .then((data) => {
+        // data grooming
+        functions.logger.info("data derek: ", typeof data);
+        functions.logger.info("data derek: ", data.data.children);
+        // for (let i = 0; i < data.children.length; ++i) {
+        //   const currPost = data.children[i].data;
+        //   data.children[i] = {
+        //     title: currPost["title"],
+        //     ups: currPost["ups"],
+        //     downs: currPost["downs"],
+        //     subreddit: currPost["subreddit_name_prefixed"],
+        //     text: currPost["selftext"],
+        //     url: currPost["url"],
+        //   };
+        // }
+        // functions.logger.info("data derek: ", data.children);
+        return data.data.children;
+      });
 };
 
 // // Create and Deploy Your First Cloud Functions
