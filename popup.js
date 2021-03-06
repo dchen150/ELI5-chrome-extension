3fe6a784f48d461b272ee80b356ae23eade2b4fd
flora chen <flora1591@hotmail.com>
2021-03-05 23:48:31 -0800

    function added to get selected text

--- popup.js
+++ popup.js
@@ -1,24 +1,18 @@
-// Initialize butotn with users's prefered color
-let changeColor = document.getElementById("changeColor");
+/* The function that finds and returns the selected text */
+var funcToInject = function() {
+  var range = window.getSelection().getRangeAt(0);
+  var selectedText = range.cloneContents().textContent;
+  console.log(selectedText);
+  return selectedText;
+};
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
+/* This line converts the above function to string
+ * (and makes sure it will be called instantly) */
+var jsCodeStr = ';(' + funcToInject + ')();';
+
+window.addEventListener('DOMContentLoaded', function() {
+
+    chrome.tabs.executeScript({
+        code: jsCodeStr
+    });
 });
-
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageBackgroundColor,
-  });
-});
-
-// The body of this function will be execuetd as a content script inside the
-// current page
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-  });
-}
