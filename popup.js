94d641971fb8cec2708af3cedb19e959d439c89a
Truman Hung <truman.hung@sap.com>
2021-03-06 08:49:31 -0800

    Finish up frontend logic

--- popup.js
+++ popup.js
@@ -5,8 +5,9 @@ const getSelectedText = function () {
     const {baseOffset, focusOffset} = window.getSelection();
     const selected = Math.abs(baseOffset - focusOffset) > 0;
     let selectedText = selected ? window.getSelection().getRangeAt(0).cloneContents().textContent || "" : ""
-    selectedText = selectedText.replace(/[\r\n]+/g, " ");
+    selectedText = selectedText.replace(/[\r\n]+/g, " ").trim();
     chrome.storage.sync.set({selectedText}); // storing the selected text globally
+
 };
 
 /* This line converts the above function to string
@@ -14,49 +15,81 @@ const getSelectedText = function () {
 const jsCodeWrapper = callbackFunc => ';(' + callbackFunc + ')();';
 
 
+function generateQueryDisplay(selectedText) {
+    const resultWrapper = document.querySelector(".resultWrapper");
+    resultWrapper.innerHTML = '';   // clear
+    const queryDisplayTemplate = document.querySelector("#queryDisplay");
+    let queryDisplay = queryDisplayTemplate.content.cloneNode(true);
+    queryDisplay.querySelector("#query").innerText = selectedText;
+    resultWrapper.appendChild(queryDisplay);
+
+    const resultItemTemplate = document.querySelector("#resultItem");
+    [...Array(3).keys()].forEach(i => {
+        let resultItem = resultItemTemplate.content.cloneNode(true);
+        resultItem.querySelector(".resultContent").innerText = `APPEND RESULT HERE ${i}`;
+        resultWrapper.appendChild(resultItem)
+    });
+}
+
 // Wrap pop-up related event here!!
 window.addEventListener('DOMContentLoaded', function () {
+    const caption = document.querySelector("#caption")
+    const settings = document.querySelector("#settings")
+    const searchTerm = document.querySelector('#searchTerm');
+    const searchBtn = document.querySelector('.searchButton');
+    const response = document.querySelector('#response');
+    const clearBtn = document.querySelector('.close-icon');
+    const resultWrapper = document.querySelector(".resultWrapper");
+
     // Select text
     chrome.tabs.executeScript({
         code: jsCodeWrapper(getSelectedText)
     });
 
     // Header caption
-    const caption = document.querySelector("#caption")
     caption.addEventListener('click', () => chrome.tabs.create({url: GitHubLink}));
 
     // Header settings logo
-    const settings = document.querySelector("#settings")
+
     settings.addEventListener('click', () => chrome.tabs.create({'url': 'chrome://extensions/?options=' + chrome.runtime.id}));
 
     // You search for...
-    // const queryTerm = document.querySelector('#query');
     chrome.storage.sync.get(['selectedText'], function (result) {
         if (result.selectedText) {
-            // append query display
-            const resultWrapper = document.querySelector(".resultWrapper");
-            const queryDisplayTemplate = document.querySelector("#queryDisplay");
-            let queryDisplay = queryDisplayTemplate.content.cloneNode(true);
-            queryDisplay.querySelector("#query").innerText = result.selectedText;
-            resultWrapper.appendChild(queryDisplay);
-
-            const resultItemTemplate = document.querySelector("#resultItem");
-            [...Array(3).keys()].forEach(i => {
-                let resultItem = resultItemTemplate.content.cloneNode(true);
-                resultItem.querySelector(".resultContent").innerText = `APPEND RESULT HERE ${i}`;
-                resultWrapper.appendChild(resultItem)
-            });
-
+            generateQueryDisplay(result.selectedText);
+            response.style.display = 'block';
+            searchTerm.style.display = 'none';
+            searchBtn.style.display = 'none'
+            clearBtn.style.display = 'inline-block';
         }
     });
 
+    // Clear button
+    clearBtn.addEventListener('click', () => {
+        resultWrapper.innerHTML = '';   // clear
+        response.style.display = 'none';
+        searchTerm.style.display = 'block';
+        searchBtn.style.display = 'block'
+        clearBtn.style.display = 'none';
+    });
 
     // Search button
-    const searchBtn = document.querySelector('.searchButton');
     searchBtn.addEventListener('click', function () {
-        chrome.tabs.executeScript({
-            code: `alert("Search btn clicked");`
-        })
+        // generateQueryDisplay(searchTerm.value)
+        // response.style.display = 'block';
+        // searchTerm.style.display = 'none';
+        // searchBtn.style.display = 'none'
+        // searchBtn.disabled = true;
+        // searchTerm.value = null
     }, false);
+
+    // Search bar
+    searchTerm.addEventListener('input', (event) => {
+        let selectedText = event.target.value.replace(/[\r\n]+/g, " ").trim();
+        chrome.storage.sync.set({selectedText}); // storing the selected text globally
+        searchBtn.disabled = !selectedText
+
+    });
+
 
 });
